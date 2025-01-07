68794237db0acd4345a2137a53fd585a37ae5da5
Yurii Potapov <yuapotapov@edu.hse.ru>
2025-01-07 23:32:05 +0300

    fix naming

--- arenachess/setup.py
+++ arenachess/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="arenachess",
+    name="chessarena-client",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
